0eb0065836a3263381de6a4794c24bb668932921
Sabvg <Sabela.vazquez.gago@gmail.com>
2021-02-05 02:02:07 +0100

    Update listado.component.ts

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -1,4 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
 import { Component} from '@angular/core';
 
 @Component({
@@ -6,30 +5,10 @@ import { Component} from '@angular/core';
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
-// export class ListadoComponent implements OnInit {
 export class ListadoComponent{
-  // constructor() {
-  //   console.log('constructor');
-  //  }
-
-  // ngOnInit(): void {
-  //   console.log('ngOnInit');
-  // }
-
   heroes: string[] = ['Spiderman', 'Iroman', 'Hulk', 'Thor', 'Capitán América'];
   heroeBorrado: string = '';
-
   borrarHeroe() {
-    // console.log('borrado heroe...');
-    // this.heroes = [];
-    // this.heroes.shift(); //borra el primer elemento del array y lo retorna
-
-    //const heroeBorrado = this.heroes.shift();
-    //console.log('Héroe borrado: ' + heroeBorrado);
-    // this.heroeBorrado = heroeBorrado;
     this.heroeBorrado = this.heroes.shift() || ''; //si el shift es undefined entonces retorna un string vacío
-
   }
-
 }
-
