866128cde175f47416c13905b4a692781561e365
Sabvg <Sabela.vazquez.gago@gmail.com>
2021-02-05 02:04:08 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-// import { ContadorComponent } from './contador/contador.component';
-// import { HeroeComponent } from './heroes/heroe/heroe.component';
-// import { ListadoComponent } from './heroes/listado/listado.component';
-//lo quitamos pq ya lo llamamos desde  heroes.module.ts 
-
-// TAREA:
-// Crear un module llamado ContadorModule
-// declaraciones y exportaciones
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // ContadorComponent,
-    // HeroeComponent,
-    // ListadoComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,4 +16,4 @@ import { ContadorModule } from './contador/contador.module';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
